bcc8907ebc8c41fc521d24693bb7db5269992986
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-30 21:19:10 +0300

    Добавляет решение Задачи - 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const liElement = data => {
+  const liItem = document.createElement('li');
+  liItem.textContent = data;
+  return liItem;
+};
+
+const liElementsCreating = data => data.map(liElement);
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+ingredientsRef.append(...liElementsCreating(ingredients));
